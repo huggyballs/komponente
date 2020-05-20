c347cd5f77f48f4b0b670d9f6ecd8b424557fb78
huggyballs <spomich123@gmail.com>
2020-05-20 14:50:15 +0200

    Update nfc2.py

--- nfc2.py
+++ nfc2.py
@@ -9,5 +9,10 @@ target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212
 print(target)
 tag = nfc.tag.activate(clf, target)
 print(tag)
+tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
+print(tag2)
+tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
+print(tag3)
+clf.close
 
 print("Kraj")
