5f2a744fdbeb29d9498a0160e72e200ca9430195
huggyballs <spomich123@gmail.com>
2020-05-20 15:09:29 +0200

    Update nfc2.py

--- nfc2.py
+++ nfc2.py
@@ -4,7 +4,6 @@ from nfc.clf import RemoteTarget
 clf = nfc.ContactlessFrontend()
 clf.open
 assert clf.open('tty:AMA0') is True
-clf.close
 
 target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
 print(target)
@@ -14,6 +13,5 @@ tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
 print(tag2)
 tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
 print(tag3)
-clf.close
 
 print("Kraj")
