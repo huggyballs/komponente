39a14c40ffb01187ce9c4af88547f379ed28c988
huggyballs <spomich123@gmail.com>
2020-05-21 14:14:05 +0200

    Update nfcread.py

--- nfcread.py
+++ nfcread.py
@@ -2,11 +2,13 @@ import nfc
 from nfc.clf import RemoteTarget
 
 clf = nfc.ContactlessFrontend()
+clf.open('tty:AMA0')
+
 
 target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
 print(target)
-tag1 = nfc.tag.activate(clf, target)
-print(tag1)
+tag = nfc.tag.activate(clf, target)
+print(tag)
 tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
 print(tag2)
 tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
