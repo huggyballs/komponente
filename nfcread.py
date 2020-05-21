9da498304d511407f0eb3cd91b9e52bf50749181
huggyballs <spomich123@gmail.com>
2020-05-21 14:50:45 +0200

    Update nfcread.py

--- nfcread.py
+++ nfcread.py
@@ -4,14 +4,15 @@ from nfc.clf import RemoteTarget
 clf = nfc.ContactlessFrontend()
 clf.open('tty:AMA0')
 
-
-target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
-print(target)
-tag = nfc.tag.activate(clf, target)
-print(tag)
-tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
-print(tag2)
-tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
-print(tag3)
-clf.close()
-print("Gotovo je gotovo!")
+try:
+    target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
+    print(target)
+    tag = nfc.tag.activate(clf, target)
+    print(tag)
+    tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
+    print(tag2)
+    tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
+    print(tag3)
+finally:
+    clf.close()
+    print("Gotovo je gotovo!")
