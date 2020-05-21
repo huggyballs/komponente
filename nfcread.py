cb0342e80f6b09c473e0cedc6351ecce0b394b43
huggyballs <spomich123@gmail.com>
2020-05-21 13:58:16 +0200

    Create nfcread.py

---
+++ nfcread.py
@@ -0,0 +1,14 @@
+import nfc
+from nfc.clf import RemoteTarget
+
+clf = nfc.ContactlessFrontend('usb')
+
+target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
+print(target)
+tag1 = nfc.tag.activate(clf, target)
+print(tag1)
+tag2 = clf.connect(rdwr={'on-connect': lambda tag: False})
+print(tag2)
+tag3 = clf.connect(rdwr={'on-connect': lambda tag: True})
+print(tag3)
+print("Gotovo je gotovo!")
