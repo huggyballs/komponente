e53499aebd5f82e3445f9d8b8db697bbd52d477f
huggyballs <spomich123@gmail.com>
2020-05-21 15:05:31 +0200

    Create pynfcexample.py

---
+++ pynfcexample.py
@@ -0,0 +1,14 @@
+from pynfc import Nfc, Desfire, Timeout
+
+n = Nfc("pn532_uart:/dev/ttyAMA0")
+
+DESFIRE_DEFAULT_KEY = b'\x00' * 8
+MIFARE_BLANK_TOKEN = b'\xFF' * 1024 * 4
+
+for target in n.poll():
+    try:
+        print(target.uid, target.auth(DESFIRE_DEFAULT_KEY if type(target) == Desfire else MIFARE_BLANK_TOKEN))
+        print("Uspjeh!")
+    except TimeoutException:
+        print("Neuspjeh!")
+        pass
