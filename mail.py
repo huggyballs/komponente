78d1b182964df449934e60b12a58d368ca00206c
huggyballs <spomich123@gmail.com>
2020-09-13 12:33:09 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -9,15 +9,26 @@ message = """\
 Subject: Hi there
 
 This message is sent from Python."""
-server = smtplib.SMTP('smtp.gmail.com', port)
-try:
-    server = smtplib.SMTP('smtp.gmail.com', port)
-    server.ehlo()
-    server.starttls()
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-except Exception as e:
-    print(e)
-finally:
-    server.quit()
+message2 = """\
+Subject: Hi there
 
+This message is sent from Python.2"""
+
+def sendEmail(msg):
+    server = smtplib.SMTP('smtp.gmail.com', port)
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', port)
+        server.ehlo()
+        server.starttls()
+        server.login(sender_email, password)
+        if msg == 1
+            server.sendmail(sender_email, receiver_email, message)
+        else:
+            server.sendmail(sender_email, receiver_email, message2)
+    except Exception as e:
+        print(e)
+    finally:
+        server.quit()
+
+odabir = input("Odaberite koju poruku slati, 1 ili 2?")
+sendEmail(odabir)
